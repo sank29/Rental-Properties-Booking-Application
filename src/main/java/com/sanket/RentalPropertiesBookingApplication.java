5929ddf9cd141596d011390120c69f6e70406b37
sanket <wankhdes29@gmail.com>
2023-04-17 17:04:19 +0530

    starter code

---
+++ src/main/java/com/sanket/RentalPropertiesBookingApplication.java
@@ -0,0 +1,13 @@
+package com.sanket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalPropertiesBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalPropertiesBookingApplication.class, args);
+	}
+
+}
